5383eb689c9f07467955569d05e4af1f44edabf5
sk8erece1 <44900269+sk8erece1@users.noreply.github.com>
2018-11-14 20:27:30 +0300

    Update unban.js

--- komutlar/unban.js
+++ komutlar/unban.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
   .setColor(0xFF0000)
   .setTimestamp()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`unban` adlı komutu özel mesajlarda kullanamazsın.')
+  .addField(':warning: Uyarı :warning:', '`unban` Adlı Komutu Özel Mesajlarda Kullanamazsın.')
   return message.author.sendEmbed(ozelmesajuyari); }
   let guild = message.guild
   let reason = args.slice(1).join(' ');
@@ -16,9 +16,9 @@ exports.run = (client, message, args) => {
   client.unbanAuth = message.author;
   let user = args[0];
   let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`mod-log` kanalını bulamıyorum.');
-  if (reason.length < 1) return message.reply('Ban kaldırma sebebini yazmalısın.');
-  if (!user) return message.reply('Banı kaldırılacak kişinin ID numarasını yazmalısın.').catch(console.error);
+  if (!modlog) return message.reply('`mod-log` Kanalını Bulamıyorum.');
+  if (reason.length < 1) return message.reply('Ban kaldırma Sebebini Yazmalısın.');
+  if (!user) return message.reply('Banı Kaldırılacak Kişinin ID Numarasını Yazmalısın.').catch(console.error);
   message.guild.unban(user);
 
   const embed = new Discord.RichEmbed()
@@ -40,6 +40,6 @@ exports.conf = {
 
 exports.help = {
   name: 'unban',
-  description: 'İstediğiniz kişinin banını kaldırır.',
-  usage: 'unban [kullanıcı] [sebep]'
+  description: 'İstediğiniz Kişinin Banını Kaldırır.',
+  usage: 'unban [Kullanıcı] [Sebep]'
 };
