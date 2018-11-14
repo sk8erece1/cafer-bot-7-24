f3b2d48683f76212c27f722b4ad2d8d5d27ee373
sk8erece1 <44900269+sk8erece1@users.noreply.github.com>
2018-11-14 20:28:47 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
 
   if (!message.guild) {
-    return message.author.send('`temizle` komutu sadece sunucularda kullanılabilir.');
+    return message.author.send('`temizle` Komutu Sadece Sunucularda Kullanılabilir.');
   }
   let mesajsayisi = parseInt(args.join(' '));
-  if (mesajsayisi.length < 1) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin.')
-  if (mesajsayisi > 100) return message.channel.send('100 adetden fazla mesaj silemem!');
+  if (mesajsayisi.length < 1) return message.channel.send('Kaç Mesaj Silmem Gerektiğini Belirtmedin.')
+  if (mesajsayisi > 100) return message.channel.send('100 Adetden Fazla Mesaj Silemem!');
   message.channel.bulkDelete(mesajsayisi + 1);
-  message.channel.send(mesajsayisi +' adet mesaj sildim!')
+  message.channel.send(mesajsayisi +' Adet Mesaj Sildim!')
 };
 
 exports.conf = {
@@ -20,6 +20,6 @@ exports.conf = {
 
 exports.help = {
   name: 'temizle',
-  description: 'Belirlenen miktar mesajı siler.',
-  usage: 'temizle <temizlenecek mesaj sayısı>'
+  description: 'Belirlenen Miktar Mesajı Siler.',
+  usage: 'temizle <Temizlenecek Mesaj Sayısı>'
 };
