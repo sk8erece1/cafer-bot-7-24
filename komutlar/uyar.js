faa840fc52d024124556b35ff36346ae88b9e7ae
sk8erece1 <44900269+sk8erece1@users.noreply.github.com>
2018-11-14 20:24:06 +0300

    Update uyar.js

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -6,19 +6,19 @@ exports.run = (client, message, args) => {
   .setColor(0xFF0000)
   .setTimestamp()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`uyar` adlı komutu özel mesajlarda kullanamazsın.')
+  .addField(':warning: Uyarı :warning:', '`uyar` Adlı Komutu Özel Mesajlarda Kullanamazsın.')
   return message.author.sendEmbed(ozelmesajuyari); }
   let guild = message.guild
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
   let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`mod-log` kanalını bulamıyorum.');
-  if (reason.length < 1) return message.reply('Uyarı sebebini yazmalısın.');
-  if (message.mentions.users.size < 1) return message.reply('Kimi uyaracağını yazmalısın.').catch(console.error);
+  if (!modlog) return message.reply('`mod-log` Kanalını Bulamıyorum.');
+  if (reason.length < 1) return message.reply('Uyarı Sebebini Yazmalısın.');
+  if (message.mentions.users.size < 1) return message.reply('Kimi Uyaracağını Yazmalısın.').catch(console.error);
   const embed = new Discord.RichEmbed()
   .setColor(0x00AE86)
   .setTimestamp()
-  .addField('Eylem:', 'Uyarı verme')
+  .addField('Eylem:', 'Uyarı Verme')
   .addField('Kullanıcı:', `${user.username}#${user.discriminator}`)
   .addField('Yetkili:', `${message.author.username}#${message.author.discriminator}`)
   .addField('Sebep', reason);
@@ -35,6 +35,6 @@ exports.conf = {
 
 exports.help = {
   name: 'uyar',
-  description: 'İstediğiniz kişiyi uyarır.',
-  usage: 'uyar [kullanıcı] [sebep]'
+  description: 'İstediğiniz Kişiyi Uyarır.',
+  usage: 'uyar [Kullanıcı] [Sebep]'
 };
