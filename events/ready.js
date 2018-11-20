038617b7a93e2bb4279e8015468b79465fbbdca9
sk8erece1 <44900269+sk8erece1@users.noreply.github.com>
2018-11-20 18:27:31 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`${prefix}yardım Herhangi Bir Sorun Olursa 《EVĪ̇ИÐÂR─ßЦÐÂҚĈĪ》#0137`);
+  client.user.setGame(`${prefix}yardım Efendim wo0LF#9703`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
